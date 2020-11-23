af2a7bc939df0c9c9e8f64a4d0f4bab49a73c3c3
Daniel Cintron <69943020+DevDan13@users.noreply.github.com>
2020-11-23 16:16:27 -0500

    created package.json and installed inquirer as a dependancy.

--- employeetracker.js
+++ employeetracker.js
@@ -1,8 +1,8 @@
-var mysql = require("mysql");
-var inquirer = require("inquirer");
+const mysql = require("mysql");
+const inquirer = require("inquirer");
 
 // create the connection information for the sql database
-var connection = mysql.createConnection({
+const connection = mysql.createConnection({
   host: "localhost",
 
   // Your port; if not 3306
@@ -12,8 +12,8 @@ var connection = mysql.createConnection({
   user: "root",
 
   // Your password
-  password: "",
-  database: "greatBay_DB"
+  password: "password",
+  database: "employee_db"
 });
 
 // connect to the mysql server and sql database
